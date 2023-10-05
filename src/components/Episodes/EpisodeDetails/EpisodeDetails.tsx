a1d45f2a26ca73325d4094d0461e8c71e27e579e
regvud <silveron.regvud@gmail.com>
2023-10-05 23:16:10 +0300

    no-redux api call for characters for each episode details

--- src/components/Episodes/EpisodeDetails/EpisodeDetails.tsx
+++ src/components/Episodes/EpisodeDetails/EpisodeDetails.tsx
@@ -5,33 +5,30 @@ import {IEpisode} from "../../../interfaces";
 
 const EpisodeDetails = () => {
     const [episode, setEpisode] = useState<IEpisode>(null)
-    const [fetchIds, setFetchIds] = useState<string[]>([])
     const {state} = useLocation();
     const {id} = useParams();
 
-    useEffect(() => {
-        if (!state) {
-            episodesService.byID(+id).then(({data}) => setEpisode(data))
-        } else {
-            setEpisode(state)
-        }
-    }, [id]);
+    if (!state) {
+        episodesService.byID(+id).then(({data}) => setEpisode(data))
+    }
 
-    useEffect(() => {
-        episode?.characters.map(value => {
-            const lastIndex = value.lastIndexOf('/')
-            setFetchIds(prevState => [...prevState, value.slice(lastIndex + 1, value.length)])
-        })
-    }, [episode]);
 
+    // const [fetchIds, setFetchIds] = useState<string[]>([])
+    // useEffect(() => {
+    //     episode?.characters.map(value => {
+    //         const lastIndex = value.lastIndexOf('/')
+    //         setFetchIds(prevState => [...prevState, value.slice(lastIndex + 1, value.length)])
+    //     })
+    // }, [episode]);
+    console.log(episode?.characters)
     return (
         <div>
-            <h2>{episode?.id}</h2>
-            <h2>{episode?.episode}</h2>
-            <h2>{episode?.name}</h2>
-            <h2>{episode?.created}</h2>
-            <h2>{episode?.air_date}</h2>
-            <h2>{episode?.url}</h2>
+            <h2>id: {state ? state.id : episode?.id}</h2>
+            <h2>episode: {state ? state.episode : episode?.episode}</h2>
+            <h2>name: {state ? state.name : episode?.name}</h2>
+            <h2>created: {state ? state.created : episode?.created}</h2>
+            <h2>air_date: {state ? state.air_date : episode?.air_date}</h2>
+            <h2>url: {state ? state.url : episode?.url}</h2>
         </div>
     )
         ;
